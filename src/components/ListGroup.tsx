b307fae6c203aacd98e017b0e33bb7138c5a0130
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-11 23:26:41 +0300

    modified theListGroup file

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,14 @@
 function ListGroup() {
   let items = ["New York", "san francisco", "Tokyo", "London", "Paris"];
-  items = [];
 
-
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  }
 
   return (
     <>
       <h1>List</h1>
-      {getMessage(  )}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li className="list-group-item"  key={item} onClick={() => console.log("Clicked")}>
             {item}
           </li>
         ))}
