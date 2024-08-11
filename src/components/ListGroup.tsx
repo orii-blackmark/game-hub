eba843623de05c92ceba91f5766b7f3cd81275a2
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-11 23:56:45 +0300

    modified the selector panel

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "san francisco", "Tokyo", "London", "Paris"];
 
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item"  key={item} onClick={() => console.log("Clicked")}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
